2e70dfb506095ae7688bd4b74eeda1e557507939
rushikeshdighe-cmyk <rushikeshdighe@gmail.com>
2026-01-04 15:02:10 +0530

    Forloop

--- src/IterationStatement/WhileLoop2.java
+++ src/IterationStatement/WhileLoop2.java
@@ -1,14 +1,17 @@
 package IterationStatement;
 
-public class Whileloop {
+public class WhileLoop2 {
 
 	public static void main(String[] args) {
 
+		int sum=0;
 		int i=1;
 		
-		while(i<=5) {
+		while (i<=100) {
 			
-			System.out.println(i);
+			sum = sum+i;
+			
+			System.out.println(sum);
 			i++;
 		}
 	}
--- src/IterationStatement/WhileLoop3.java
+++ src/IterationStatement/WhileLoop3.java
@@ -1,14 +1,15 @@
 package IterationStatement;
 
-public class Whileloop {
+public class WhileLoop3 {
 
 	public static void main(String[] args) {
 
 		int i=1;
 		
 		while(i<=5) {
+			int square=i*i;
 			
-			System.out.println(i);
+			System.out.println("square of "+ i+ "="+square);
 			i++;
 		}
 	}
